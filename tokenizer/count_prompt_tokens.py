c0359d0a83797367b8e946067607febf2f32f274
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2023-02-13 13:28:51 -0600

    Add tokenizer\count_prompt_tokens.py

---
+++ tokenizer/count_prompt_tokens.py
@@ -0,0 +1,18 @@
+"""
+This function takes either a string or an array of tokens as its prompt_or_prompts argument. If the argument is a string, the function splits it by spaces and returns the length of the resulting list. If the argument is a list of tokens, the function loops through the list, checking each item to see if it's a string. If it is, the function splits it by spaces and adds the length of the resulting list to total_tokens. If it's not a string, it assumes it's a nested list of tokens and loops through it in the same manner.
+"""
+
+
+def count_prompt_tokens(prompt_or_prompts):
+    total_tokens = 0
+    if isinstance(prompt_or_prompts, str):
+        total_tokens += len(prompt_or_prompts.split())
+    elif isinstance(prompt_or_prompts, list):
+        for prompt in prompt_or_prompts:
+            if isinstance(prompt, str):
+                total_tokens += len(prompt.split())
+            elif isinstance(prompt, list):
+                for tokens in prompt:
+                    if isinstance(tokens, str):
+                        total_tokens += len(tokens.split())
+    return total_tokens
