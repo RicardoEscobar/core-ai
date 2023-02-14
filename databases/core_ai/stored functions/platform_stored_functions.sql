0358c799714c9e996d4abacd792d8b47b5b2c3ed
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2023-02-14 10:50:27 -0600

    Add update and delete functions to databases\core_ai\stored functions\platform_stored_functions.sql

--- databases/core_ai/stored functions/platform_stored_functions.sql
+++ databases/core_ai/stored functions/platform_stored_functions.sql
@@ -17,3 +17,24 @@ CREATE OR REPLACE FUNCTION "user".insert_platform(name text, description text)
         VALUES ($1, $2)
         RETURNING id;
     $$;
+
+-- Create a stored procedure that will update a platform. And returns the id of the updated platform.
+CREATE OR REPLACE FUNCTION "user".update_platform(id bigint, name text, description text)
+    RETURNS bigint
+    LANGUAGE sql
+    AS $$
+        UPDATE "user".platform
+        SET name = $2, description = $3
+        WHERE id = $1
+        RETURNING id;
+    $$;
+
+-- Create a stored procedure that will delete a platform. And returns the id of the deleted platform.
+CREATE OR REPLACE FUNCTION "user".delete_platform(id bigint)
+    RETURNS bigint
+    LANGUAGE sql
+    AS $$
+        DELETE FROM "user".platform
+        WHERE id = $1
+        RETURNING id;
+    $$;
