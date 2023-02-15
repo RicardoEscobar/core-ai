c9602ac9e2f252bfbb29fabfc510866c97495be3
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2023-02-15 12:24:00 -0600

    Add "fine-tunning".sample table constraints

--- databases/core_ai/core_ai_tables_schemas.sql
+++ databases/core_ai/core_ai_tables_schemas.sql
@@ -408,32 +408,32 @@ ALTER TABLE IF EXISTS model.training
 ALTER TABLE IF EXISTS "fine-tunning".sample
     ADD FOREIGN KEY (summary_id)
     REFERENCES "fine-tunning".summary (id) MATCH SIMPLE
-    ON UPDATE NO ACTION
-    ON DELETE NO ACTION
+    ON UPDATE CASCADE
+    ON DELETE SET NULL
     NOT VALID;
 
 
 ALTER TABLE IF EXISTS "fine-tunning".sample
     ADD FOREIGN KEY (account_id)
     REFERENCES "user".account (id) MATCH SIMPLE
-    ON UPDATE NO ACTION
-    ON DELETE NO ACTION
+    ON UPDATE CASCADE
+    ON DELETE SET NULL
     NOT VALID;
 
 
 ALTER TABLE IF EXISTS "fine-tunning".sample
     ADD FOREIGN KEY (previous_sample_id)
     REFERENCES "fine-tunning".sample (id) MATCH SIMPLE
-    ON UPDATE NO ACTION
-    ON DELETE NO ACTION
+    ON UPDATE CASCADE
+    ON DELETE SET NULL
     NOT VALID;
 
 
 ALTER TABLE IF EXISTS "fine-tunning".sample
     ADD FOREIGN KEY (model_id)
     REFERENCES model.model (id) MATCH SIMPLE
-    ON UPDATE NO ACTION
-    ON DELETE NO ACTION
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
     NOT VALID;
 
 -- Restart all table sequences.
