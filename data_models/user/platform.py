1d31871abddd1c91278a15a797ec641d2cdb8ab5
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2023-02-23 21:23:57 -0600

    Add save_platforms class method data_models\user\platform.py

--- data_models/user/platform.py
+++ data_models/user/platform.py
@@ -30,6 +30,9 @@ class Platform:
     def as_tuple(self):
         return (self._id if self._id else 'DEFAULT', self.name, self.description)
 
+    def as_insert_value(self):
+        return (self.name, self.description)
+
     def get_id(self, connection: psycopg.connection = None):
         """
         This method is used to get the id of the platform.
@@ -71,19 +74,20 @@ class Platform:
         4. The "save_platforms" method is used to save a list of platform objects into the database.
         """
         # Convert the list of Platform objects into a tuple of tuples
-        platforms_tuples = (platform.as_tuple() for platform in platforms)
+        # make a list comprehension to get the tuple of each object, extract only name and description.
+        params_seq = [platform.as_insert_value() for platform in platforms]
 
         if connection:
             # Open a cursor to perform database operations
             with connection.cursor() as cursor:
-                query = f"""INSERT INTO {Platform.table_name} VALUES %s ON CONFLICT (name) DO UPDATE SET description = EXCLUDED.description RETURNING *;"""
-                print(query)
+                query = f"""INSERT INTO {Platform.table_name} (id, name, description) VALUES (DEFAULT, %s, %s) ON CONFLICT (name) DO UPDATE SET description = EXCLUDED.description RETURNING *;"""
+
                 cursor.executemany(
-                    query, params_seq=platforms_tuples, returning=True)
+                    query, params_seq=params_seq, returning=True)
 
                 connection.commit()
                 all_rows = cursor.fetchall()
 
                 # set the _id of the object
-                for row in all_rows:
-                    platforms[all_rows.index(row)]._id = row['id']
+                for platform, row in zip(platforms, all_rows):
+                    platform._id = row[0]
