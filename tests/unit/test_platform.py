e5bf7de6a820608796111fb0cde9dcc23222d740
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2023-02-24 13:37:11 -0600

    Rename model folder

---
+++ tests/unit/test_platform.py
@@ -0,0 +1,57 @@
+"""
+Unit tests for the platform module.
+"""
+import os
+import unittest
+import psycopg
+from dotenv import load_dotenv
+import controller.pythonpath
+from model.user.platform import Platform
+
+
+class TestPlatform(unittest.TestCase):
+    """
+    This class is used to test the platform module.
+    """
+
+    def setUp(self):
+        """
+        This method is used to set up the test environment.
+        """
+        load_dotenv()
+
+        # Connect to an existing database
+        self.connection = psycopg.connect(
+            dbname=os.environ.get('DB_NAME'),
+            user=os.environ.get('DB_USER'),
+            password=os.environ.get('DB_PASSWORD'),
+            host=os.environ.get('DB_HOST'),
+            port=os.environ.get('DB_PORT')
+        )
+
+        # Create Platform objects
+        self.platform1 = Platform(
+            name='Twitch', description="""Twitch is a live streaming video platform owned by Twitch Interactive, a subsidiary of Amazon.""")
+        self.platform2 = Platform(
+            name='YouTube', description='YouTube is an American online video-sharing platform headquartered in San Bruno, California.')
+        self.platform3 = Platform(
+            name='VRChat', description='VRChat is a massively multiplayer online virtual reality social platform developed and published by VRChat Inc.')
+
+        # Save the platform objects into a list
+        self.platforms = [self.platform1,  self.platform2,  self.platform3]
+
+    def test_platform_str_repr(self):
+        """
+        This method is used to test the platform data model.
+        """
+        # Test the __str__ method
+        expected = """Platform(_id=None, name='Twitch', description="Twitch is a live streaming video platform owned by Twitch Interactive, a subsidiary of Amazon.")"""
+        self.assertEqual(str(self.platform1), expected)
+
+        # Test the __repr__ method
+        expected = """Platform(_id=None, name='Twitch', description="Twitch is a live streaming video platform owned by Twitch Interactive, a subsidiary of Amazon.")"""
+        self.assertEqual(repr(self.platform1), expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
