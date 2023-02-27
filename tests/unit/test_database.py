e0b452ecb2ae1fa2526b2d1819776b39e4cb8b7a
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2023-02-27 11:48:30 -0600

    Add tests\unit\test_database.py

---
+++ tests/unit/test_database.py
@@ -0,0 +1,70 @@
+import os
+import unittest
+from unittest.mock import patch, Mock
+from model.database import Database
+from dotenv import load_dotenv
+import psycopg
+
+
+class TestDatabase(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        # load the environment variables
+        load_dotenv()
+
+        # Connect to an existing database
+        cls.connection = psycopg.connect(
+            dbname=os.environ.get('DB_NAME'),
+            user=os.environ.get('DB_USER'),
+            password=os.environ.get('DB_PASSWORD'),
+            host=os.environ.get('DB_HOST'),
+            port=os.environ.get('DB_PORT')
+        )
+
+    @classmethod
+    def tearDownClass(cls):
+        # Clean up environment variables
+        del os.environ['DB_NAME']
+        del os.environ['DB_USER']
+        del os.environ['DB_PASSWORD']
+        del os.environ['DB_HOST']
+        del os.environ['DB_PORT']
+
+    def setUp(self):
+        # Create a new database instance for each test
+        self.db = Database()
+
+    def tearDown(self):
+        # Close the database connection after each test
+        del self.db
+
+    def test_execute(self):
+        # Test the execute method
+        result = self.db.execute('SELECT 1')
+        self.assertEqual(result, [(1,)])
+
+    def test_execute_with_values(self):
+        # Test the execute method with values
+        result = self.db.execute('SELECT %s', (1,))
+        self.assertEqual(result, [(1,)])
+
+    def test_execute_many(self):
+        # Test the execute_many method
+        result = self.db.execute_many('SELECT %s', [(1,), (2,), (3,)])
+        self.assertEqual(result, [(1,), (2,), (3,)])
+
+    def test_execute_script(self):
+        # Test the execute_script method
+        result = self.db.execute_script('SELECT 1')
+        self.assertEqual(result, [(1,)])
+
+    @patch('builtins.open', new_callable=Mock, return_value=Mock(read=Mock(return_value='SELECT 1')))
+    def test_execute_script_file(self, mock_open):
+        # Test the execute_script_file method
+        result = self.db.execute_script_file('/path/to/script.sql')
+        self.assertEqual(result, [(1,)])
+        mock_open.assert_called_once_with('/path/to/script.sql', 'r')
+
+
+if __name__ == '__main__':
+    unittest.main()
