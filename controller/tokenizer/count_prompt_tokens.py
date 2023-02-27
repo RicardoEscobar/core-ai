9b3a81517c65d870c9aac06bf7b27fc2a5c4c4ca
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2023-02-27 13:44:01 -0600

    Adds explanation to docstring controller\tokenizer\count_prompt_tokens.py

--- controller/tokenizer/count_prompt_tokens.py
+++ controller/tokenizer/count_prompt_tokens.py
@@ -1,13 +1,5 @@
 """
 This function takes either a string or an array of tokens as its prompt_or_prompts argument.
-
-If the argument is a string, the function splits it by spaces and returns the length of the resulting list.
-
-If the argument is a list of tokens, the function loops through the list, checking each item to see if it's a string.
-
-If it is, the function splits it by spaces and adds the length of the resulting list to total_tokens.
-
-If it's not a string, it assumes it's a nested list of tokens and loops through it in the same manner.
 """
 
 
@@ -17,6 +9,10 @@ from typing import Union, List
 def count_prompt_tokens(prompt_or_prompts: Union[str, List[Union[str, List[str]]]]) -> int:
     """
     Counts the total number of tokens in a prompt or a list of prompts.
+    - If the argument is a string, the function splits it by spaces and returns the length of the resulting list.
+    - If the argument is a list of tokens, the function loops through the list, checking each item to see if it's a string.
+    - If it is, the function splits it by spaces and adds the length of the resulting list to total_tokens.
+    - If it's not a string, it assumes it's a nested list of tokens and loops through it in the same manner.
 
     Args:
         prompt_or_prompts: A string or a list of strings/lists containing the prompts.
