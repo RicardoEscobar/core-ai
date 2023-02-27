5092aec0bd088113192f15192f6bf070eabb0dad
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2023-02-27 13:47:15 -0600

    Replace commets with docstrings in test methods tests\unit\test_database.py

--- tests/unit/test_database.py
+++ tests/unit/test_database.py
@@ -40,28 +40,28 @@ class TestDatabase(unittest.TestCase):
         del self.db
 
     def test_execute(self):
-        # Test the execute method
+        """Test the execute method"""
         result = self.db.execute('SELECT 1')
         self.assertEqual(result, [(1,)])
 
     def test_execute_with_values(self):
-        # Test the execute method with values
+        """Test the execute method with values"""
         result = self.db.execute('SELECT %s', (1,))
         self.assertEqual(result, [(1,)])
 
     def test_execute_many(self):
-        # Test the execute_many method
+        """Test the execute_many method"""
         result = self.db.execute_many('SELECT %s', [(1,), (2,), (3,)])
         self.assertEqual(result, [(1,), (2,), (3,)])
 
     def test_execute_script(self):
-        # Test the execute_script method
+        """Test the execute_script method"""
         result = self.db.execute_script('SELECT 1')
         self.assertEqual(result, [(1,)])
 
     @patch('builtins.open', new_callable=mock_open, read_data='SELECT 1')
     def test_execute_script_file(self, mock_file):
-        # Test the execute_script_file method
+        """Test the execute_script_file method"""
         result = self.db.execute_script_file(
             'databases/core_ai/tables/user_platform.sql')
         self.assertEqual(result, [(1,)])
