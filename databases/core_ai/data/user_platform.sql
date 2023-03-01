31d885b72baaa5dda49dac52831fe0420307ded5
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2023-03-01 10:28:19 -0600

    Remove unneccesary or irrelevant platforms from databases\core_ai\data\user_platform.sql

--- databases/core_ai/data/user_platform.sql
+++ databases/core_ai/data/user_platform.sql
@@ -2,14 +2,7 @@
 
 INSERT INTO "user".platform (name, description)
 VALUES 
-('VRChat','VRChat is a social VR platform where users can create and share their own virtual worlds, avatars, and experiences.'),
-('Discord','Discord is a free and secure all-in-one voice and text chat for gamers.'),
-('Twitter','Twitter is an American microblogging and social networking service on which users post and interact with messages known as "tweets".'),
-('Twitch','Twitch is a live streaming video platform owned by Twitch Interactive, a subsidiary of Amazon.'),
-('YouTube','YouTube is an American online video-sharing platform headquartered in San Bruno, California.'),
-('Instagram','Instagram is an American photo and video sharing social networking service owned by Facebook, Inc.'),
-('TikTok','TikTok is a Chinese video-sharing social networking service owned by ByteDance, a Beijing-based internet technology company founded in 2012 by Zhang Yiming.'),
-('LinkedIn','LinkedIn is an American business and employment-oriented service that operates via websites and mobile apps.'),
-('Google','Google is an American multinational technology company that specializes in Internet-related services and products, which include online advertising technologies, search engine, cloud computing, software, and hardware.')
+('Core AI','Core AI is a platform for interacting with AI powered chatbots, NPCs, and other virtual characters.'),
+('VRChat','VRChat is a social VR platform where users can create and share their own virtual worlds, avatars, and experiences.')
 ON CONFLICT (name) DO UPDATE SET description = EXCLUDED.description
 RETURNING *;
