008598b01ddc8783ebfbdd62317faae46b42444a
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2023-03-12 10:40:47 -0600

    Add sql queries to create user.account table

---
+++ databases/core_ai/tables/user/account/create_table_user_account.sql
@@ -0,0 +1,28 @@
+/*
+Create table user.account
+This table contains the different user accounts on different platforms.
+
+I make a distinction betrween user accounts and users themselves, they are not the same to me.
+
+A user can have multiple accounts on different platforms, but an account can only belong to one user.
+
+This table is used to identify the user making the request to the API.
+
+The user_id column is used to identify the user making the request to the API.
+
+The platform_id column is used to identify the platform the user is using to make the request to the API.
+
+The name column is used to identify the user account on the platform the user is using to make the request to the API.
+
+The name column is a unique identifier representing the end-user account on a given platform, which can help OpenAI to monitor and detect abuse.
+
+The combination of the name and platform_id columns is unique.
+*/
+
+CREATE TABLE IF NOT EXISTS "user".account
+(
+    id serial PRIMARY KEY,
+    name text NOT NULL,
+    platform_id smallserial NOT NULL
+    CONSTRAINT UNIQUE (name, platform_id)
+);
