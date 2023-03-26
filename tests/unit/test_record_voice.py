cb65838de35a17d610bcfb08759b267d673bc23d
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2023-03-26 13:19:11 -0600

    Fix broken chatgpt code tests\unit\test_record_voice.py

--- tests/unit/test_record_voice.py
+++ tests/unit/test_record_voice.py
@@ -2,7 +2,7 @@ import unittest
 from unittest.mock import patch
 import pyaudio
 from typing import List
-from record_voice import record_voice
+from controller.conversation.record_voice import record_voice
 
 
 class TestRecordVoice(unittest.TestCase):
@@ -12,7 +12,11 @@ class TestRecordVoice(unittest.TestCase):
         mock_stream = mock_open.return_value
         mock_stream.read.return_value = b"sample data"
 
-        # Call record_voice() and capture the frames
+        # Add a side effect to the stream's read() method to simulate
+        # the user interrupting the recording
+        mock_stream.read.side_effect = KeyboardInterrupt
+
+        # Call record_voice() and capture the frames using the mock stream
         frames = record_voice()
 
         # Assert that frames is a non-empty list
