1ef056183f7bfbcd3d45520204e79bc51ae8479d
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2023-06-29 13:47:18 -0600

    Add unit tests for chat gui tests\unit\test_chat.py

---
+++ tests/unit/test_chat.py
@@ -0,0 +1,13 @@
+"""Unit tests for view/chat.py"""
+import unittest
+
+
+class TestChat(unittest.TestCase):
+    """Unit tests for chat.py"""
+
+    def test_chat(self):
+        """Test chat"""
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
