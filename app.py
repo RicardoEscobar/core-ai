28a5671f70935cba077f4286fee6768330d9bb84
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2023-07-01 10:00:16 -0600

    Add app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+"""This is the start of the GUI and OpenAI chatbot."""
+from view.chat import create_gui
+
+
+def main():
+    """Main function."""
+    create_gui()
+
+if __name__ == "__main__":
+    main()
