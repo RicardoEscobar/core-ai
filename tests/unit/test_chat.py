43c0313f98a689e64cb804e7f4758820cffb30b4
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2023-07-01 10:01:58 -0600

    Add emtpy unit tests tests\unit\test_chat.py

--- tests/unit/test_chat.py
+++ tests/unit/test_chat.py
@@ -1,13 +1,25 @@
-"""Unit tests for view/chat.py"""
+"""Unit tests for the chat module."""
+
+# add the project root directory to the system path
+if __name__ == '__main__':
+    from pathlib import Path
+    project_directory = Path(__file__).parent.parent
+    print(project_directory)
+    import sys
+    # sys.path.insert(0, str(project_directory))
+    sys.path.append(str(project_directory))
+    print(sys.path)
+
 import unittest
+from unittest.mock import patch
+
+from view.chat import send_text, send_command
 
 
 class TestChat(unittest.TestCase):
-    """Unit tests for chat.py"""
+    """Test the chat module."""
 
-    def test_chat(self):
-        """Test chat"""
+    @patch("view.chat.send_text")
+    def test_send_text(self):
+        """Test the send_text function."""
         pass
-
-if __name__ == '__main__':
-    unittest.main()
