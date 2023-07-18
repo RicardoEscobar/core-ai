bd8a494578e775f9b672157488bc33ff9e97bb07
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2023-07-18 16:08:04 -0600

    Add convert_wav() function to convert wav files for windows at: controller\natural_voice.py

--- controller/natural_voice.py
+++ controller/natural_voice.py
@@ -2,6 +2,8 @@
 import os
 from typing import Union
 from pathlib import Path
+import subprocess
+import logging
 
 from elevenlabs import generate, play, voices, save
 from elevenlabs.api import Voices, Voice, VoiceSettings
@@ -11,6 +13,17 @@ from controller.conversation.load_openai import load_openai
 # Load environment variables from .env file
 load_openai()
 
+def convert_wav(input_file, output_file, skip_existing=False):
+    if skip_existing and os.path.exists(output_file):
+        print(f"Skipping conversion because output file '{output_file}' already exists.")
+        return
+
+    command = ['ffmpeg', '-i', input_file, '-c:a', 'pcm_s16le', output_file]
+    try:
+        subprocess.check_output(command, stderr=subprocess.STDOUT)
+        print("Conversion completed successfully.")
+    except subprocess.CalledProcessError as error:
+        print("Conversion failed with error:", error.output)
 
 def generate_multilingual(
     text: str = "¡Hola! Mi nombre es Arnold, encantado de conocerte!",
@@ -19,11 +32,28 @@ def generate_multilingual(
 ):
     """The eleven_multilingual_v1 model supports multiple languages, including
     English, German, Polish, Spanish, Italian, French, Portuguese, and Hindi."""
+    # Set logging level
+    logging.basicConfig(level=logging.ERROR)
+
+    # Generate the audio.
     audio = generate(text=text, voice=voice, model="eleven_multilingual_v1")
 
     # Save the audio to a file.
     audio_file_path = Path(audio_file)
-    save(audio, str(audio_file_path.resolve()))
+    non_riff_audio = Path(audio_file_path.stem + "_non_riff" + audio_file_path.suffix).resolve()
+
+    logging.info("Saving non RIFF audio to '%s'.", non_riff_audio)
+    save(audio, str(non_riff_audio))
+
+    try:
+        logging.info("Converting non RIFF audio to WAV format.")
+        # Convert the audio file to WAV format or else it won't play on Windows.
+        convert_wav(non_riff_audio, audio_file_path, skip_existing=True)
+    except Exception as error:
+        logging.error(error)
+    else:
+        # Remove the non-riff audio file.
+        non_riff_audio.unlink()
 
 
 def get_voices():
