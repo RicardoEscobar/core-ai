03a97031dfb5699dd8696e8503900915a441befe
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2023-07-27 11:54:11 -0600

    Removes VRChat text limit exeption and formats: controller\vrchat.py

--- controller/vrchat.py
+++ controller/vrchat.py
@@ -19,10 +19,11 @@ from controller.create_logger import create_logger
 # Create logger
 module_logger = create_logger(
     logger_name=__name__,
-    logger_filename='vrchat.log',
-    log_directory='logs',
-    add_date_to_filename=False
+    logger_filename="vrchat.log",
+    log_directory="logs",
+    add_date_to_filename=False,
 )
+
 
 class VRChat:
     """This class controls the VRChat client."""
@@ -55,36 +56,41 @@ class VRChat:
         emote_dict = {
             "none": 0,
             "wave": 1,
-            "applause": {"emote": 2, "duration": 5}, # loops, needs to be stopped
+            "applause": {"emote": 2, "duration": 5},  # loops, needs to be stopped
             "point": 3,
-            "cheer": {"emote": 4, "duration": 5},# loops, needs to be stopped
-            "dance": 5, # loops, needs to be stopped
+            "cheer": {"emote": 4, "duration": 5},  # loops, needs to be stopped
+            "dance": 5,  # loops, needs to be stopped
             "backflip": 6,
             "sad": 7,
-            "die": 8, # loops, needs to be stopped
-            "t-pose": 9, # loops, needs to be stopped
-            "hk-walk": {"emote": 10, "duration": 5}, # loops, needs to be stopped
-            "dont-start-now": 11, # loops, needs to be stopped
-            "ashton-salt-lake": 12, # loops, needs to be stopped
-            "blinding-lights": 13, # loops, needs to be stopped
-            "skinwalker": 14, # loops, needs to be stopped
-            "laugh": {"emote": 15, "duration": 5}, # loops, needs to be stopped
-            "moonwalking": 16, # loops, needs to be stopped
-            "armup": 17, # loops, needs to be stopped
-            "afrohouse": 18, # loops, needs to be stopped
-            "ashton-boardwalk": 19, # loops, needs to be stopped
-            "breakdance": 20, # loops, needs to be stopped
-            "a1-dance": 21, # loops, needs to be stopped
-            "bbd": 22, # loops, needs to be stopped
-            "balletspin": 23, # loops, needs to be stopped
-            "calculated" : {"emote": 24, "duration": 3}, # loops, needs to be stopped
+            "die": 8,  # loops, needs to be stopped
+            "t-pose": 9,  # loops, needs to be stopped
+            "hk-walk": {"emote": 10, "duration": 5},  # loops, needs to be stopped
+            "dont-start-now": 11,  # loops, needs to be stopped
+            "ashton-salt-lake": 12,  # loops, needs to be stopped
+            "blinding-lights": 13,  # loops, needs to be stopped
+            "skinwalker": 14,  # loops, needs to be stopped
+            "laugh": {"emote": 15, "duration": 5},  # loops, needs to be stopped
+            "moonwalking": 16,  # loops, needs to be stopped
+            "armup": 17,  # loops, needs to be stopped
+            "afrohouse": 18,  # loops, needs to be stopped
+            "ashton-boardwalk": 19,  # loops, needs to be stopped
+            "breakdance": 20,  # loops, needs to be stopped
+            "a1-dance": 21,  # loops, needs to be stopped
+            "bbd": 22,  # loops, needs to be stopped
+            "balletspin": 23,  # loops, needs to be stopped
+            "calculated": {"emote": 24, "duration": 3},  # loops, needs to be stopped
         }
         address = "/avatar/parameters/VRCEmote"
         # If emote_dict[emote_key] us a dictionary, save the duration and send the emote.
         if isinstance(emote_dict[emote_key], dict):
             duration = emote_dict[emote_key]["duration"]
             emote_value = emote_dict[emote_key]["emote"]
-            self.logger.debug("Sending emote '%s' with value: %d  to %s", emote_key, emote_value, address)
+            self.logger.debug(
+                "Sending emote '%s' with value: %d  to %s",
+                emote_key,
+                emote_value,
+                address,
+            )
             self.client.send_message(address, emote_value)
 
             # Wait for the amount of seconds specified at 'duration' argument, then stop the emote.
@@ -95,31 +101,46 @@ class VRChat:
                 elapsed_time = current_time - start_time
 
                 if elapsed_time >= duration + 1:
-                    self.logger.debug("Sending emote '%s' with value: %d  to %s", emote_key, 0, address)
+                    self.logger.debug(
+                        "Sending emote '%s' with value: %d  to %s",
+                        emote_key,
+                        0,
+                        address,
+                    )
                     # stop emote
                     self.client.send_message(address, 0)
                     break
         else:
             # If emote_key is in emote_dict, send emote to the VRChat client.
             self.client.send_message(address, emote_dict[emote_key])
-            self.logger.debug("Sent emote '%s' with value: %d  to %s", emote_key, emote_dict[emote_key], address)
+            self.logger.debug(
+                "Sent emote '%s' with value: %d  to %s",
+                emote_key,
+                emote_dict[emote_key],
+                address,
+            )
 
-    def send_text(self, text: str = ""):
+    def send_text(self, text: str = "", duration: float = 10.0):
+        """Wrapper for _send_text method."""
+        # VRChat limits the number of characters that can be sent to 144 per chat bubble.
+        if len(text) > self.TEXT_LIMIT:
+            self.logger.debug(
+                "Text length %d exceeds text limit %d", len(text), self.TEXT_LIMIT
+            )
+            # Split text into a list of strings with a maximum width of TEXT_LIMIT.
+            text_list = self.split_string(text, self.TEXT_LIMIT)
+            # Send each string in the list to the VRChat client.
+            self.send_text_list(text_list, duration)
+        else:
+            # Send a single text to the VRChat client.
+            self._send_text(text)
+
+    def _send_text(self, text: str = ""):
         """This method sends the text to the VRChat client."""
 
         address = "/chatbox/input"
         # If self.text is not empty, send self.text to the VRChat client.
         if text != "":
-            # VRChat limits the number of characters that can be sent to 144 per chat bubble.
-            if len(text) > self.TEXT_LIMIT:
-                self.logger.error(
-                    "Text length %d exceeds text limit %d", len(text), self.TEXT_LIMIT
-                )
-                raise ValueError(
-                    "Text length {} exceeds text limit {}".format(
-                        len(text), self.TEXT_LIMIT
-                    )
-                )
             self.logger.debug("Sending text: %s", repr(text))
             self.client.send_message(address, [text, True, False])
             self.last_sent_text = text
@@ -150,7 +171,7 @@ class VRChat:
             # resulting duration per message is proportional to the length of
             # the message relative to the total length of all the messages.
             duration_per_message = duration * (len(text) / total_length)
-            self.send_text(text)
+            self._send_text(text)
             self.logger.debug("Sleeping for %s seconds", repr(duration_per_message))
             time.sleep(duration_per_message)
 
@@ -163,19 +184,23 @@ class VRChat:
 
         # Create thread to avoid stopping the main thread when time.sleep is called.
         thread = threading.Thread(
-            target=self._send_text_list, args=(text_list, duration)
+            target=self._send_text_list,
+            args=(text_list, duration),
+            name="send_text_thread",
         )
         thread.start()
+
 
 def main():
     # Create VRChat object
     vrchat = VRChat()
 
     # Send text
-    vrchat.send_text("Hello World!")
+    vrchat._send_text("Hello World!")
 
     # Delete VRChat object
     del vrchat
+
 
 if __name__ == "__main__":
     main()
