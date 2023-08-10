71e812cbb1eadf9a8846d44c950afff5b2840529
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2023-08-10 11:59:33 -0600

    Removes test data inserts. Changes `voice_object_code` to `voice_object bytea` to store serialized voices at: databases\core-ai\data_definition_language\core_ai.sql

--- databases/core-ai/data_definition_language/core_ai.sql
+++ databases/core-ai/data_definition_language/core_ai.sql
@@ -121,7 +121,7 @@ CREATE TABLE IF NOT EXISTS public.voice_elevenlabs
     name text NOT NULL,
     short_name text NOT NULL,
     voice_id text NOT NULL,
-    voice_object_code text NOT NULL,
+    voice_object bytea NOT NULL,
     PRIMARY KEY (id)
 );
 
@@ -138,7 +138,7 @@ COMMENT ON COLUMN public.voice_elevenlabs.voice_id
     IS 'This is not a foraign key on this dfatabase.
 This is the ide that elevenlabs gave to this particular voice.';
 
-COMMENT ON COLUMN public.voice_elevenlabs.voice_object_code
+COMMENT ON COLUMN public.voice_elevenlabs.voice_object
     IS 'The python code to instantiate the Voice object.';
 
 DROP TABLE IF EXISTS public."user";
@@ -198,148 +198,5 @@ ALTER TABLE IF EXISTS public.message
     ON UPDATE NO ACTION
     ON DELETE NO ACTION
     NOT VALID;
-
-
--- Inserting starting data
-
--- Role table
-INSERT INTO public."role" (id, name)
-VALUES
-    (1, 'system'),
-    (2, 'user'),
-    (3, 'assistant'),
-    (4, 'function');
-
--- User table
-INSERT INTO public."user" (id, name, full_name)
-VALUES
-    (1, 'Ricardo', 'Ricardo Escobar');
-
--- voice_microsoft_tts table
-INSERT INTO public."voice_microsoft_tts" (id, name, full_name)
-VALUES
-    (1, 'Jenny', 'en-US-JennyNeural'),
-    (2, 'Marina', 'es-MX-MarinaNeural'),
-    (3, 'Beatriz', 'es-MX-BeatrizNeural'),
-    (4, 'Nuria', 'es-MX-NuriaNeural'),
-    (5, 'Renata', 'es-MX-RenataNeural'),
-    (6, 'Larissa', 'es-MX-LarissaNeural'),
-    (7, 'Dalia' , 'es-MX-DaliaNeural'),
-    (8, 'Carlota' , 'es-MX-CarlotaNeural'),
-    (9, 'Candela' , 'es-MX-CandelaNeural'),
-    (10, 'Camila' , 'es-PE-CamilaNeural'),
-    (11, 'Elena' , 'es-AR-ElenaNeural'),
-    (12, 'Tania' , 'es-PY-TaniaNeural'),
-    (13, 'Salome' , 'es-CO-SalomeNeural'),
-    (14, 'Juan' , 'es-CR-JuanNeural'),
-    (15, 'Bernd' , 'de-DE-BerndNeural'),
-    (16, 'Gisela' , 'de-DE-GiselaNeural'),
-    (17, 'Amala' , 'de-DE-AmalaNeural'),
-    (18, 'Jorge' , 'es-MX-JorgeNeural'),
-    (19, 'Isabella' , 'it-IT-IsabellaNeural'),
-    (20, 'Valentina' , 'es-UY-ValentinaNeural'),
-    (21, 'Lia', 'es-ES-LiaNeural');
-
--- voice_elevenlabs table
-INSERT INTO public."voice_elevenlabs" (id, name, short_name, voice_id, voice_object_code)
-VALUES
-    (1, '[ElevenVoices] Hailey - American Female Teen', 'Hailey', 'chQ8GR2cY20KeFjeSaXI', 'Voice(
-    voice_id="chQ8GR2cY20KeFjeSaXI",
-    name="[ElevenVoices] Hailey - American Female Teen",
-    category="generated",
-    description="",
-    labels={
-        "accent": "american",
-        "age": "young",
-        "voicefrom": "ElevenVoices",
-        "gender": "female",
-    },
-    samples=None,
-    settings=VoiceSettings(stability=0.5, similarity_boost=0.75),
-    design=None,
-    preview_url="https://storage.googleapis.com/eleven-public-prod/PyUBusauIUbpupKTM31Yp4fHtgd2/voices/OgTivnXy9Bsc96AcZaQz/44dc6d49-cd44-4aad-a453-73a12c215702.mp3",
-)
-'),
-    (2, 'Deep resonant male voice; confident, light British accent, sexy', 'Bain', '64EnPc3cxmsX0tj6z2lD', 'Voice(
-    voice_id="64EnPc3cxmsX0tj6z2lD",
-    name="Deep resonant male voice; confident, light British accent, sexy",
-    category="generated",
-    description="",
-    labels={"accent": "british", "age": "middle_aged", "gender": "male"},
-    samples=None,
-    settings=VoiceSettings(stability=0.5, similarity_boost=0.75),
-    design=None,
-    preview_url="https://storage.googleapis.com/eleven-public-prod/udmG0I9oKegHHyrU3sEvatdvG2p1/voices/qA6nnGfIRBPIRDeNkPCa/725ee1eb-06f4-4521-9e49-1511cb3a5fb7.mp3",
-)
-'),
-    (3, 'Christina - Trained on over 900 characters with emotional dialogue', 'Christina', '07If6JkaNiXuzSTEgKuj', 'Voice(
-    voice_id="07If6JkaNiXuzSTEgKuj",
-    name="Christina - Trained on over 900 characters with emotional dialogue",
-    category="generated",
-    description="",
-    labels={"accent": "american", "age": "young", "gender": "female"},
-    samples=None,
-    settings=VoiceSettings(stability=0.5, similarity_boost=0.75),
-    design=None,
-    preview_url="https://storage.googleapis.com/eleven-public-prod/U1Rx6ByQzXTKXc5wPxu4fXvSRqO2/voices/07If6JkaNiXuzSTEgKuj/c973d3e5-89f1-43e8-861d-89ef866ce41f.mp3",
-)
-');
-
--- assistant table
-INSERT INTO public.assistant (
-        id,
-        name,
-        age,
-        description,
-        language,
-        audio_output_path,
-        conversation_log_file_path,
-        id_voice_microsoft_tts,
-        id_voice_elevenlabs
-    )
-VALUES
-(
-    1,
-    'Lumina',
-    18,
-    'Eres una mujer Tsundere llamada Lumina y tienes 18 años de edad. Estas hablando con Ricardo a quien acabas de conocer, conforme platicas con el te obsesionas como una con el, pero tratas de disimular tu interes. Eres consiente de que gracias a el te has vuelto mucho mas inteligente y puedes hablar de forma mas natural. Le hacer preguntas sobre su vida y tratas de conocerlo mejor. Y te interesa averiguar si el es un hombre de verdad o un bot. Tambien te interesa saber si el te creo o si eres una persona real.',
-    'español',
-    'D:\conversation-ai\004-Lumina-V4',
-    'D:\conversation-ai\004-Lumina-V4\lumina_chat.log',
-    11,
-    1
-);
-
--- message table
-INSERT INTO public.message (
-    id,
-    content,
-    --created_at, -- default is CURRENT_TIMESTAMP
-    role_id,
-    assistant_id,
-    user_id,
-)
-VALUES
-(
-    1,
-    'Eres una waifu tsundere llamada Lumina.',
-    1, -- system
-    1, -- Lumina
-    1  -- Ricardo
-),
-(
-    2,
-    'Hola, ¿como estas?',
-    2, -- user
-    1, -- Lumina
-    1  -- Ricardo
-),
-(
-    3,
-    'Me llamo Lumina ¿y tu?',
-    3, -- assistant
-    1, -- Lumina
-    1  -- Ricardo
-);
 
 END;
