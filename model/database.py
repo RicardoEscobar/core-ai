48684d45553270e506da9062277f0c1096102672
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2023-08-17 23:03:49 -0600

    Refactor and extract load_dotenv() to module scope at: model\database.py

--- model/database.py
+++ model/database.py
@@ -10,14 +10,17 @@ from dotenv import load_dotenv
 from controller.create_logger import create_logger
 
 module_logger = create_logger(
-    logger_name='model.database',
-    logger_filename='database.log',
-    log_directory='logs/database',
+    logger_name="model.database",
+    logger_filename="database.log",
+    log_directory="logs/database",
     add_date_to_filename=False,
 )
 
+# load the environment variables
+load_dotenv()
 
-class Database():
+
+class Database:
     """
     This class is used to connect to the database and perform database operations.
     """
@@ -27,33 +30,33 @@ class Database():
         This method is used to initialize the database connection.
         """
         self.logger = module_logger
-        self.logger.info('Creating an instance of Database')
-
-        # load the environment variables
-        load_dotenv()
+        self.logger.info("Creating an instance of Database")
 
         # Connect to an existing database
         self.connection = psycopg.connect(
-            dbname=os.environ.get('DB_NAME'),
-            user=os.environ.get('DB_USER'),
-            password=os.environ.get('DB_PASSWORD'),
-            host=os.environ.get('DB_HOST'),
-            port=os.environ.get('DB_PORT')
+            dbname=os.environ.get("DB_NAME"),
+            user=os.environ.get("DB_USER"),
+            password=os.environ.get("DB_PASSWORD"),
+            host=os.environ.get("DB_HOST"),
+            port=os.environ.get("DB_PORT"),
         )
 
     def __del__(self):
         """
         This method is used to close the database connection.
         """
-        self.logger.info('Closing the database connection.')
+        self.logger.info("Closing the database connection.")
         # Close the connection
-        self.connection.close()
+        try:
+            self.connection.close()
+        except Exception as exception:
+            self.logger.error("Error closing the database connection: %s", exception)
 
     def execute(self, query: str, values: Tuple[str] = None) -> List:
         """
         This method is used to execute a query on the database.
         """
-        self.logger.info('Executing query: %s', query)
+        self.logger.info("Executing query: %s", query)
         # Open a cursor to perform database operations
         with self.connection.cursor() as cursor:
             cursor.execute(query, values)
@@ -64,15 +67,16 @@ class Database():
         """
         This method is used to execute a query on the database.
         """
-        self.logger.info('Executing query: %s', query)
+        self.logger.debug("Executing query: %s", query)
+        self.logger.debug("execute_many method Values: %s", values)
         # Open a cursor to perform database operations
         with self.connection.cursor() as cursor:
             cursor.executemany(query, params_seq=values, returning=True)
             self.connection.commit()
 
             # Fetch all the rows
-            resultset = list(cursor.fetchall())
-            self.logger.info('execute_many method Resultset: %s', resultset)
+            resultset = cursor.fetchall()
+            self.logger.info("execute_many method Resultset: %s", resultset)
 
             return resultset
 
@@ -80,7 +84,7 @@ class Database():
         """
         This method is used to execute a script on the database.
         """
-        self.logger.info('Executing script: %s', script)
+        self.logger.info("Executing script: %s", script)
         # Open a cursor to perform database operations
         with self.connection.cursor() as cursor:
             cursor.execute(script)
@@ -98,8 +102,8 @@ class Database():
         """
         This method is used to execute a script on the database.
         """
-        self.logger.info('Executing script file: %s', file_path)
-        with open(file_path, 'r', encoding='utf-8') as file:
+        self.logger.info("Executing script file: %s", file_path)
+        with open(file_path, "r", encoding="utf-8") as file:
             script = file.read()
         return self.execute_script(script)
 
@@ -108,30 +112,23 @@ class Database():
         This method is used to create the user schema.
         """
         # Create the user schema
-        self.logger.info('Creating the user schema.')
-        self.execute_script_file(
-            'databases/core_ai/schemas/user.sql')
+        self.logger.info("Creating the user schema.")
+        self.execute_script_file("databases/core_ai/schemas/user.sql")
 
         # Create the user.platform table
-        self.logger.info('Creating the user.platform table.')
-        self.execute_script_file(
-            'databases/core_ai/tables/user_platform.sql')
+        self.logger.info("Creating the user.platform table.")
+        self.execute_script_file("databases/core_ai/tables/user_platform.sql")
 
         # Insert data to the user.platform table
-        self.logger.info('Inserting the user.platform data.')
-        resultset = self.execute_script_file(
-            'databases/core_ai/data/user_platform.sql')
-        self.logger.info(
-            'Inserted rows into user.platform table: %s',  resultset)
+        self.logger.info("Inserting the user.platform data.")
+        resultset = self.execute_script_file("databases/core_ai/data/user_platform.sql")
+        self.logger.info("Inserted rows into user.platform table: %s", resultset)
 
         # Create the user.account table
-        self.logger.info('Creating the user.account table.')
-        self.execute_script_file(
-            'databases/core_ai/tables/user_account.sql')
+        self.logger.info("Creating the user.account table.")
+        self.execute_script_file("databases/core_ai/tables/user_account.sql")
 
         # Insert data to the user.account table
-        self.logger.info('Inserting the user.account data.')
-        resultset = self.execute_script_file(
-            'databases/core_ai/data/user_account.sql')
-        self.logger.info(
-            'Inserted rows into user.account table: %s',  resultset)
+        self.logger.info("Inserting the user.account data.")
+        resultset = self.execute_script_file("databases/core_ai/data/user_account.sql")
+        self.logger.info("Inserted rows into user.account table: %s", resultset)
