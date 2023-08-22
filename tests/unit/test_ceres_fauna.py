2008ee578268c865cf241c71549f0bdd90a2718e
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2023-08-22 11:10:00 -0600

    Add logger and fixes test_repr at: tests\unit\test_ceres_fauna.py

--- tests/unit/test_ceres_fauna.py
+++ tests/unit/test_ceres_fauna.py
@@ -13,7 +13,6 @@ import unittest
 import logging
 
 from controller.vtuberai.ceres_fauna import CeresFauna
-from controller.vtuber_chat import TextColor
 from controller.create_logger import create_logger
 
 
@@ -22,34 +21,46 @@ module_logger = create_logger(
     logger_name="tests.unit.test_ceres_fauna",
     logger_filename="test_ceres_fauna.log",
     log_directory="logs/tests",
-    console_logging=True,
+    console_logging=False,
     console_log_level=logging.INFO,
 )
 
+
 class TestCeresFauna(unittest.TestCase):
+    """Test the CeresFauna class"""
+
+    logger = module_logger
+
     def test_init(self):
         vtuber = CeresFauna(name="Ceres Fauna", age=18, gpt_model="gpt-4")
         self.assertEqual(vtuber.name, "Ceres Fauna")
         self.assertEqual(vtuber.age, 18)
         self.assertEqual(vtuber.gpt_model, "gpt-4")
+        self.assertEqual(vtuber.language, CeresFauna.language)
+        self.assertEqual(vtuber.personality, CeresFauna.personality)
+        self.assertEqual(vtuber.personality_type, CeresFauna.personality_type)
+        self.logger.info("Tested the __init__ method of the CeresFauna class.")
 
     def test_str(self):
         vtuber = CeresFauna(name="Ceres Fauna", age=18, gpt_model="gpt-4")
         self.assertEqual(str(vtuber), "Ceres Fauna is a 18 year old VTuberAI")
+        self.logger.info("Tested the __str__ method of the CeresFauna class.")
 
     def test_repr(self):
         vtuber = CeresFauna(name="Ceres Fauna", age=18, gpt_model="gpt-4")
-        self.assertEqual(
-            repr(vtuber),
-            "CearesFauna(name=Ceres Fauna, age=18, gpt_model=gpt-4)"
+        expected = "CeresFauna(name={}, age={}, gpt_model={}, language={}, personality={}, personality_type={})".format(
+            repr("Ceres Fauna"),
+            18,
+            repr("gpt-4"),
+            repr(CeresFauna.language),
+            repr(CeresFauna.personality),
+            repr(CeresFauna.personality_type),
         )
+        self.logger.debug("Expected: {}".format(expected))
 
-    def test_chat(self):
-        """Test the Twitch chat method of the VTuberAI"""
+        self.assertEqual(repr(vtuber), expected)
+        self.logger.info("Tested the __repr__ method of the CeresFauna class.")
 
-        # Assert that the 'Twitch' object is not None when creating a 'CeresFauna' object
-        vtuber = CeresFauna(name="Ceres Fauna", age=18, gpt_model="gpt-4")
-        self.assertIsNotNone(vtuber.twitch)
 
 if __name__ == "__main__":
     unittest.main()
