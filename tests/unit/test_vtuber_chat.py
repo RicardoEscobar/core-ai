a136b585826a8a0f40c99abfed58da464d7bb255
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2023-08-22 10:29:12 -0600

    Add test_save_chat_log unit test to: tests\unit\test_vtuber_chat.py

--- tests/unit/test_vtuber_chat.py
+++ tests/unit/test_vtuber_chat.py
@@ -76,6 +76,42 @@ class TestVTuberChat(unittest.TestCase):
         # Delete the log file
         os.remove(log_file_path)
 
+    def test_save_chat_log(self):
+        """Test the save_chat method"""
+        self.logger.info("Testing save_chat method.")
+        vtuber_chat = VTuberChat()
+
+        # Assert that the chat_log throws an error if it's not a ChatMessage, ChatSub, or ChatCommand
+        with self.assertRaises(TypeError):
+            vtuber_chat.save_chat_log(msg="Test message")
+
+        # Assert that the chat_log dictionary appends the message to the correct channel
+        mock_message = MagicMock(spec=ChatMessage)
+        mock_message.room.name = "testroom"
+        mock_message.text = "test text"
+        vtuber_chat.save_chat_log(mock_message)
+        self.logger.info(f"Created a chat_log dict: {vtuber_chat.chat_log}")
+
+        # Assert that the chat_log dictionary appends the message to the correct channel. Multiple channels and messages on this assertion.
+        mock_message2 = MagicMock(spec=ChatMessage)
+        mock_message2.room.name = "testroom2"
+        mock_message2.text = "test text2"
+        vtuber_chat.save_chat_log(mock_message2)
+
+        mock_message3 = MagicMock(spec=ChatMessage)
+        mock_message3.room.name = "testroom"
+        mock_message3.text = "test text3"
+        vtuber_chat.save_chat_log(mock_message3)
+
+        mock_message4 = MagicMock(spec=ChatMessage)
+        mock_message4.room.name = "testroom2"
+        mock_message4.text = "test text4"
+
+        
+        vtuber_chat.save_chat_log(mock_message4)
+        self.logger.info(f"Created a multiple message chat_log dict: {vtuber_chat.chat_log}")
+
+
 
 if __name__ == "__main__":
     unittest.main()
