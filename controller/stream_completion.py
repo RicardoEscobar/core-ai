8f57ee7c9735da55ff8effd9c9307f65cee65010
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2023-08-21 21:11:04 -0600

    Remake into a class StreamCompletion to: controller\stream_completion.py

--- controller/stream_completion.py
+++ controller/stream_completion.py
@@ -22,6 +22,7 @@ import openai
 from elevenlabs import generate, stream, save
 from elevenlabs.api import Voice, VoiceSettings
 import pyaudio
+import tiktoken
 
 from controller.load_openai import load_openai
 from controller.create_logger import create_logger
@@ -38,127 +39,198 @@ module_logger = create_logger(
     console_log_level=logging.INFO,
 )
 
-def completion_generator(
-    prompt,
-    max_tokens=100,
-    temperature=0.9,
-    top_p=1,
-    frequency_penalty=0,
-    presence_penalty=0,
-    stop=["\n"],
-    stream=True,
-    model="gpt-3.5-turbo",
-    yield_characters: Tuple[str] = None,
-) -> str:
-    """This generator function yields the next completion from the OpenAI API from a stream mode openai completion. Each time a sentence is completed, the generator yields the sentence. To detect the end of a sentence, the generator looks for a period, question mark, or exclamation point at the end of the sentence. If the sentence is not complete, then the generator yields None. If the generator yields None, then the caller should call the generator again to get the next completion. If the generator yields a sentence, then the caller should call the generator again to get the next completion. The generator will yield None when the stream is complete."""
 
-    if yield_characters is None:
-        yield_characters = (":", ";", ",", ".", "¿", "?", "¡", "!")
+class StreamCompletion:
+    """This class handles the stream completion from the OpenAI API."""
 
-    # record the time before the request is sent
-    start_time = time.time()
+    def __init__(
+        self,
+        voice: Voice = None,
+        prompt: str = "Habla como una vtuber chilena, hablas con muchos modismos chilenos, y eres una Tsundere obsesionada con su chat y das inicio al stream. (un parrafo)",
+        gpt_model: str = "gpt-4",
+        token_threshold: int = 2000,  # 2000 tokens is half the max tokens allowed by GPT-3.5-Turbo model
+    ):
+        """Initialize the StreamCompletion class."""
+        self.logger = module_logger
 
-    # send a ChatCompletion request to count to 100
-    response = openai.ChatCompletion.create(
-        model=model,
-        messages=[
-            {
-                "role": "user",
-                "content": prompt,
-            }
-        ],
-        temperature=temperature,
-        stream=stream,  # again, we set stream=True
-    )
+        if voice is None:
+            self.vtuber_voice = Voice(
+                voice_id="chQ8GR2cY20KeFjeSaXI",
+                name="[ElevenVoices] Hailey - American Female Teen",
+                category="generated",
+                description="",
+                labels={
+                    "accent": "american",
+                    "age": "young",
+                    "voicefrom": "ElevenVoices",
+                    "gender": "female",
+                },
+                samples=None,
+                settings=VoiceSettings(stability=0.5, similarity_boost=0.75),
+                design=None,
+                preview_url="https://storage.googleapis.com/eleven-public-prod/PyUBusauIUbpupKTM31Yp4fHtgd2/voices/OgTivnXy9Bsc96AcZaQz/44dc6d49-cd44-4aad-a453-73a12c215702.mp3",
+            )
 
-    # create variables to collect the stream of chunks
-    collected_chunks = []
-    collected_deltas = []
-    sentence = ""
-    # iterate through the stream of events
-    for chunk in response:
-        chunk_time = time.time() - start_time  # calculate the time delay of the chunk
-        collected_chunks.append(chunk)  # save the event response
-        chunk_delta = chunk["choices"][0]["delta"]  # extract the delta content
-        collected_deltas.append(chunk_delta)  # save the delta content
-        # if chunk_delta dict contains "role" key or is an emtpy dict, then is not a sentence
-        if "role" in chunk_delta or not chunk_delta:
+        self.prompt = prompt
+        self.gpt_model = gpt_model
+        self.token_threshold = token_threshold
+
+    def generate_completion(
+        self,
+        prompt: str = None,
+        gpt_model: str = None,
+        voice: Voice = None,
+        audio_dir_path: str = "./audio",
+    ):
+        """Generate a completion from the OpenAI API."""
+
+        if prompt is None:
+            prompt = self.prompt
+
+        if gpt_model is None:
+            gpt_model = self.gpt_model
+
+        if voice is None:
+            voice = self.vtuber_voice
+
+        self.audio_stream = generate(
+            text=self.completion_generator(self.prompt, gpt_model=gpt_model),
+            voice=self.vtuber_voice,
+            model="eleven_multilingual_v1",
+            stream=True,
+        )
+
+        # Play the audio stream
+        audio_stream = stream(self.audio_stream)
+
+        # Create the filename as a Path object
+        mp3_file_path = Path(audio_dir_path) / StreamCompletion.get_mp3_filename(
+            self.prompt
+        )
+
+        # Create the folder if it does not exist
+        mp3_file_path.parent.mkdir(parents=True, exist_ok=True)
+
+        print(f"Saving audio to {mp3_file_path.resolve()}")
+
+        # Save the audio stream to a file
+        save(audio_stream, str(mp3_file_path.resolve()))
+
+    def completion_generator(
+        self,
+        prompt: str = None,
+        temperature=0.9,
+        stream=True,
+        gpt_model=None,
+        yield_characters: Tuple[str] = None,
+    ) -> str:
+        """This generator function yields the next completion from the OpenAI API from a stream mode openai completion. Each time a sentence is completed, the generator yields the sentence. To detect the end of a sentence, the generator looks for a period, question mark, or exclamation point at the end of the sentence. If the sentence is not complete, then the generator yields None. If the generator yields None, then the caller should call the generator again to get the next completion. If the generator yields a sentence, then the caller should call the generator again to get the next completion. The generator will yield None when the stream is complete."""
+
+        if prompt is None:
+            prompt = self.prompt
+
+        if gpt_model is None:
+            gpt_model = self.gpt_model
+
+        if yield_characters is None:
+            yield_characters = (".", "?", "!", "\n", ":", ";")
+
+        # record the time before the request is sent
+        start_time = time.time()
+
+        # send a ChatCompletion request to count to 100
+        response = openai.ChatCompletion.create(
+            model=gpt_model,
+            messages=[
+                {
+                    "role": "user",
+                    "content": prompt,
+                }
+            ],
+            temperature=temperature,
+            stream=stream,  # again, we set stream=True
+        )
+
+        # create variables to collect the stream of chunks
+        collected_chunks = []
+        collected_deltas = []
+        sentence = ""
+
+        # iterate through the stream of events
+        for chunk in response:
+            chunk_time = (
+                time.time() - start_time
+            )  # calculate the time delay of the chunk
+            collected_chunks.append(chunk)  # save the event response
+            chunk_delta = chunk["choices"][0]["delta"]  # extract the delta content
+            collected_deltas.append(chunk_delta)  # save the delta content
+            # if chunk_delta dict contains "role" key or is an emtpy dict, then is not a sentence
+            if "role" in chunk_delta or not chunk_delta:
+                module_logger.debug(
+                    "Message received {:.2f} seconds after request: {}".format(
+                        chunk_time, chunk_delta
+                    )
+                )
+                continue
+            else:
+                # if the chunk is a sentence, then yield the sentence
+                if chunk_delta["content"] != "":
+                    sentence += chunk_delta["content"]
+
+                # check if the sentence is complete, yield the sentence
+                if chunk_delta["content"].endswith(yield_characters):
+                    response = sentence
+                    sentence = ""
+                    print(response, end="", flush=True)
+                    if response.endswith((" ", ".", "?", "!")):
+                        yield response
+                    else:
+                        yield response + " "
+
             module_logger.debug(
                 "Message received {:.2f} seconds after request: {}".format(
                     chunk_time, chunk_delta
                 )
             )
-            continue
-        else:
-            # if the chunk is a sentence, then yield the sentence
-            if chunk_delta["content"] != "":
-                sentence += chunk_delta["content"]
 
-            # check if the sentence is complete, yield the sentence
-            if chunk_delta["content"].endswith(yield_characters):
-                response = sentence
-                sentence = ""
-                print(response, end="", flush=True)
-                if response.endswith((" ", ".", "?", "!")):
-                    yield response
-                else:
-                    yield response + " "
-
+        # print the time delay and text received
         module_logger.debug(
-            "Message received {:.2f} seconds after request: {}".format(
-                chunk_time, chunk_delta
-            )
+            "Full response received {:.2f} seconds after request".format(chunk_time)
         )
+        full_reply_content = "".join([m.get("content", "") for m in collected_deltas])
+        module_logger.debug("Full conversation received: %s", full_reply_content)
 
-    # print the time delay and text received
-    module_logger.debug(
-        "Full response received {:.2f} seconds after request".format(chunk_time)
-    )
-    full_reply_content = "".join([m.get("content", "") for m in collected_deltas])
-    module_logger.debug("Full conversation received: %s", full_reply_content)
+    @staticmethod
+    def get_mp3_filename(text: str) -> str:
+        """Return a filename for the mp3 file."""
+        filename = re.sub(r"[^\w\s-]", "", text[:100]).strip()
+        filename = re.sub(r"[-\s]+", "-", filename)
+        filename = re.sub(r"[.,:;¿?¡!]", "", filename)
+        # Add timestamp to filename
+        filename = f"{datetime.now().strftime('%Y%m%d_%H%M%S')}_{filename}.mp3"
+        return filename
 
-def get_mp3_filename(text: str) -> str:
-    """Return a filename for the mp3 file."""
-    filename = re.sub(r"[^\w\s-]", "", text[:100]).strip()
-    filename = re.sub(r"[-\s]+", "-", filename)
-    filename = re.sub(r"[.,:;¿?¡!]", "", filename)
-    # Add timestamp to filename
-    filename = f"{datetime.now().strftime('%Y%m%d_%H%M%S')}_{filename}"
-    return filename
 
 def main():
     """Run the main function."""
+    enc = tiktoken.get_encoding("cl100k_base")
+    assert enc.decode(enc.encode("hello world")) == "hello world"
 
-    vtuber_voice = Voice(
-        voice_id="chQ8GR2cY20KeFjeSaXI",
-        name="[ElevenVoices] Hailey - American Female Teen",
-        category="generated",
-        description="",
-        labels={
-            "accent": "american",
-            "age": "young",
-            "voicefrom": "ElevenVoices",
-            "gender": "female",
-        },
-        samples=None,
-        settings=VoiceSettings(stability=0.5, similarity_boost=0.75),
-        design=None,
-        preview_url="https://storage.googleapis.com/eleven-public-prod/PyUBusauIUbpupKTM31Yp4fHtgd2/voices/OgTivnXy9Bsc96AcZaQz/44dc6d49-cd44-4aad-a453-73a12c215702.mp3",
-    )
+    # To get the tokeniser corresponding to a specific model in the OpenAI API:
+    enc = tiktoken.encoding_for_model("gpt-4")
 
-    prompt = "Habla como una vtuber chilena, hablas con muchos modismos chilenos, y eres una Yandere obsesionada con su chat. (un parrafo, GRITANDO)"
+    # Get the encoding object for the "cl100k_base" encoding
+    enc = tiktoken.get_encoding("cl100k_base")
 
-    audio_stream = generate(
-        text=completion_generator(prompt, model="gpt-4"),
-        voice=vtuber_voice,
-        model="eleven_multilingual_v1",
-        stream=True,
-    )
+    # Encode a string into tokens
+    tokens = enc.encode("Hello, world!")
 
-    audio_stream = stream(audio_stream)
+    # Count the number of tokens
+    num_tokens = len(tokens)
 
-    save(audio_stream, f"{get_mp3_filename(prompt)}.mp3")
+    print(f"The string has {num_tokens} tokens.")
 
 
 if __name__ == "__main__":
-    main()
+    stream_completion = StreamCompletion()
+    stream_completion.generate_completion()
